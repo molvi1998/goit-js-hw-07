24c58693c38ec38b60fd30687807d6f7ae52f9a9
Mozgarik <91159717+Mozgarik@users.noreply.github.com>
2023-04-30 18:41:18 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    gallery: document.querySelector(".gallery")
+}
 
-console.log(galleryItems);
+
+
+
+const galleryItem =  galleryItems.map(item => {
+    return (
+        `
+        <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+        `
+    )
+}).join("")
+
+
+refs.gallery.innerHTML = galleryItem
+refs.gallery.addEventListener("click", clickOnImg)
+
+
+
+function clickOnImg(e) {
+if(e.target.nodeName !== "IMG"){
+   return 
+}
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img
+    class="gallery__image"
+    src=${e.target.dataset.source}
+  />
+    </div>
+`)
+
+instance.show(e)
+
+const modal = document.querySelector(".modal")
+
+modal.addEventListener("click", () => {
+    instance.close()
+}) 
+
+window.addEventListener("keydown", () => {
+    if(e.target.nodeName !== "ESCAPE") {
+        instance.close()
+    }
+})
+}
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    gallery: document.querySelector(".gallery")
+}
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems.map(item => {
+    return (
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href=${item.original}>
+                <img class="gallery__image" src=${item.preview} alt=${item.description} />
+            </a>
+        </li>
+        `
+    )
+}).join("")
+
+
+refs.gallery.innerHTML = galleryItemsMarkup
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
